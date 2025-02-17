2c123b6b4ff31cc3b61df97e7af1ef7af770bafd
mmiiranda <mauricio201190b@gmail.com>
2025-02-17 16:21:38 -0300

    build: add pinia

---
+++ src/views/SearchView.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <FooterComponent />
+        <main>
+            <h2>Buscando Resultados para: {{ search  }}</h2>
+        </main>
+        <FooterComponent />
+    </div>
+</template>
+
+<script lang="ts">
+import FooterComponent from '@/components/footer/footerComponent.vue';
+import HeaderComponent from '@/components/header/headerComponent.vue';
+import { defineComponent } from 'vue'
+
+
+export default defineComponent({
+    data(){
+        return {
+            search: "olaTudoBem"
+        }
+    },
+    setup () {
+        
+
+        return {}
+    },
+    components: {
+        HeaderComponent,
+        FooterComponent
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
